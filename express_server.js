ae3a49b5d0dad023b869f0a03accaf8d7ff95812
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-02 19:54:26 +0000

    add body-parser

--- express_server.js
+++ express_server.js
@@ -2,7 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let solution = "";
+  while (solution.length < 6){
+    let thisChar = chars[Math.round(Math.random() * 62)];
+    if (thisChar !== undefined){
+      solution += thisChar;
+    }
+  }
+  return solution;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,6 +35,15 @@ app.get('/urls.json', (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
