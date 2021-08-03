1c83af365a9e38465b569c0ebb5ec61089d81bb2
cupokyle <kyleforsythcomedy@gmail.com>
2021-08-03 00:57:18 +0000

    esLint server file

--- express_server.js
+++ express_server.js
@@ -10,14 +10,14 @@ app.set("view engine", "ejs");
 const generateRandomString = function() {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   let solution = "";
-  while (solution.length < 6){
+  while (solution.length < 6) {
     let thisChar = chars[Math.round(Math.random() * 62)];
-    if (thisChar !== undefined){
+    if (thisChar !== undefined) {
       solution += thisChar;
     }
   }
   return solution;
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -40,13 +40,13 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;  
+  urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);     // Respond with 'Ok' (we will replace this)
 });
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  if (!urlDatabase[req.params.shortURL]){
-    return res.status(404).send('Sorry, can\'t find that page.')
+  if (!urlDatabase[req.params.shortURL]) {
+    return res.status(404).send('Sorry, can\'t find that page.');
   }
   res.redirect(longURL);
 });
@@ -62,5 +62,5 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
-})
+  console.log(`Example app listening on port ${PORT}!`);
+});
